80c57ef4cca4a0af3433b0a0efd7eeb14fe24ac3
Frederik Arvedsen <frederikarvedsen@Frederiks-MacBook-Pro.local>
2020-10-15 10:35:31 +0200

    4.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var express = require("express")
-const cors = require("cors")
-    var app = express(),
-    port = process.env,PORT || 3000;
-    var i = 0
-
-    
+var express = require('express')
+const cors = require('cors')
+  var app = express(),
+  port = process.env.PORT || 3000;
+  var i = 0
+  
