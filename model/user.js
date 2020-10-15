b0ae3558d8a136740ae33571af09c04922c31b1e
Frederik Arvedsen <frederikarvedsen@Frederiks-MacBook-Pro.local>
2020-10-15 10:26:46 +0200

    1.

---
+++ model/user.js
@@ -0,0 +1,51 @@
+class user {
+    constructor(firstname, lastname, age, email, password){
+        this.firstname = firstname
+        this.lastname = lastname
+        this.age = age
+        this.email = email
+        this.password = password
+    }
+}
+
+class paymentUser extends user{
+    constructor(user){
+        super(user.firstname, user.age)
+    }
+}
+
+class creditCard extends paymentUser {
+    constructor(user, cardnumber, cvc) {
+        super(user.firstname, user.age)
+        this.cardnumber = cardnumber
+        this.cvc = cvc
+    }
+}
+
+
+class freeUser extends user {
+    constructor(user, trialperiod){
+        super(user.firstname, user.age)
+        this.trialperiod = trialperiod
+    }
+} 
+
+class interest extends user {
+    constructor(user, interest){
+        super(user.firstname, user.age)
+        this.interest = interest
+    }
+}
+
+class match extends user {
+    constructor(user, matchId) {
+        super(user.firstname, user.age)
+        this.match = matchId
+    }
+}
+
+
+class image extends user {
+    constructor()
+}
+
