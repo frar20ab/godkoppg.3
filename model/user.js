eef3a2b16ee19f21264d7aa1b8053ab48af6d964
Frederik Arvedsen <frederikarvedsen@Frederiks-MacBook-Pro.local>
2020-10-15 13:10:06 +0200

    j

--- model/user.js
+++ model/user.js
@@ -1,16 +1,18 @@
 class user {
-    constructor(firstname, lastname, age, email, password){
+    constructor(firstname, lastname, gender, age, location, email, interest){
         this.firstname = firstname
         this.lastname = lastname
+        this.gender = gender
         this.age = age
+        this.location = location
         this.email = email
-        this.password = password
+        this.interest = []
     }
 }
 
 class paymentUser extends user{
     constructor(user, credidCardInfo){
-        super(user)
+        super(user.firstname, user.lastname, user.gender, user.age, user.location, user.email)
         this.credidCardInfo = credidCardInfo 
     }
 }
@@ -29,24 +31,22 @@ class creditCard extends paymentUser {
 
 class freeUser extends user {
     constructor(user, trialperiod){
-        super(user)
+        super(user.firstname, user.lastname, user.gender, user.age, user.location, user.email)
         this.trialperiod = trialperiod
     }
 } 
 
 class interest extends user {
-    constructor(user, basketball, running, cooking){
-        super(user.firstname, user.age)
-        this.basketball = basketball
-        this.running = running
-        this.cooking = cooking
+    constructor(user, interest){
+        super(user)
+        user.interest = interest
     }
 }
 
 class match extends user {
-    constructor(female, male) {
-        this.female = female
-        this.male = male
+    constructor(match1, match2) {
+        this.match1 = match1
+        this.match2 = match2
     }
 }
 
@@ -64,14 +64,34 @@ var imageX = //find billede og set path ind
 var imageY = //find billede og set path ind
 
 
-var userY = new user ("Sandra", "Nielsen", 19, "sn@gmail.com", "12345678",)
+var userX = new user ("Sandra", "Nielsen", "Female", 19, "Østerbro", "sn@gmail.com", "12345678", imageX)
+var credit = new creditCard ("Sandra", "Nielsen", "1234123412341234", "21/12", 124)
+var sandra = new paymentUser (userX, interestSandra, creditCard)
+var interestSandra = new Interest (userX, "Running")
 
 
-var userX = new user ("Peter", "jacobsen", 22, "pj@gmail.com", "12345678")
+var userY = new user ("Peter", "Jacobsen", "Male", 22, "Nørrebro", "pj@gmail.com", "12345678", imageY)
+
+new interest(userX, "Running", "Cooking", "Religion")
+new interest(userY, "Basketball", "Running", "Sleep")
+
+
+var testmatch = new match(userX.firstname, userY.firstname)
+var testMatch2 = new match("Sandra", "Peter")
+
+
+var userArray = []
+userArray [userX, userY]
+
+var interestsArray = []
+interestsArray = [userX.interest, userY.interest]
+
+
+var matchArray = []
+matchArray = [testMatch1, testMatch2]
 
 
 
 
-
-module.exports = user;
+module.exports = {userArray, interestsArray, matchArray};
 
